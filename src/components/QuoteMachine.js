2269da1e21a6a30602a99621fcdd0e539535c5c4
ali1129js <ali1129js@gmail.com>
2018-12-30 10:18:52 +0100

    fixed api call with axios

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -2,37 +2,57 @@
  * @Author: Ali
  * @Date:   2018-12-24T12:03:11+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-24T15:34:04+01:00
+ * @Last modified time: 2018-12-30T10:16:17+01:00
  */
-import React, { Component } from 'react'
-
-const URL = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+import React, { Component } from "react";
+import axios from "axios";
 
 class QuoteMachine extends Component {
-  constructor(){
-    super()
-  this.getRandQuote = this.getRandQuote.bind(this)
+  constructor() {
+    super();
+    this.state = {
+      quoteText: null,
+      quoteAuthor: null,
+      color: "white"
+    };
+    this.getRandQuote = this.getRandQuote.bind(this);
   }
-  getRandQuote(){
-    fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data[0])
-    })
+  componentDidMount() {
+    this.getRandQuote();
   }
-  render(){
+  getRandQuote() {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          quoteText: res.data.quoteText,
+          quoteAuthor: res.data.quoteAuthor,
+          color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+        });
+      });
+  }
+
+  render() {
+    const { quoteText, quoteAuthor, color } = this.state;
     return (
-      <div className="">
+      <div className="" style={{ background: color }}>
         QuoteMachine
-        <button onClick={this.getRandQuote}>
-          Show a random quote
-        </button>
+        <br />
+        {quoteText}
+        <br />
+        <strong> {quoteAuthor} </strong>
+        <br />
+        <button onClick={this.getRandQuote}>Show a random quote</button>
       </div>
-    )
+    );
   }
 }
-export default QuoteMachine
+export default QuoteMachine;
 /*
 const URL2 = 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
-
+const URL =
+  "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
 */
