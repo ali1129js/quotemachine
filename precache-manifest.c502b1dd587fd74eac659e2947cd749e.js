3d6290899ea1bafee147f84ccc66d1b2d85aff2b
ali1129js <ali1129js@gmail.com>
2019-01-03 17:14:18 +0100

    Updates

---
+++ precache-manifest.c502b1dd587fd74eac659e2947cd749e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5723bcc471d699f346",
+    "url": "/quotemachine/static/js/runtime~main.fa5723bc.js"
+  },
+  {
+    "revision": "937a691c168039451070",
+    "url": "/quotemachine/static/js/main.937a691c.chunk.js"
+  },
+  {
+    "revision": "0f1f65699dd8e0635158",
+    "url": "/quotemachine/static/js/1.0f1f6569.chunk.js"
+  },
+  {
+    "revision": "937a691c168039451070",
+    "url": "/quotemachine/static/css/main.6a3f2eff.chunk.css"
+  },
+  {
+    "revision": "0f1f65699dd8e0635158",
+    "url": "/quotemachine/static/css/1.c565573c.chunk.css"
+  },
+  {
+    "revision": "d814af6062c774d4c1e5f15b7c9b0985",
+    "url": "/quotemachine/index.html"
+  }
+];
